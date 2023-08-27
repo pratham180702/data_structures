58fe460f3157ba69a5fbcb4122364a238161d19b
unknown <prathamkakkad18@gmail.com>
2023-08-27 22:01:58 +0530

    New string files added

---
+++ Ch 8 Strings/practice set/09_impp.c
@@ -0,0 +1,11 @@
+// write a c program to find the number of occurence of each present char in the string
+
+// for example
+// if the string = Pratham
+// then the output should be:
+// P = 1
+// r = 1
+// a = 2
+// t = 1
+// h = 1
+// m = 1
